522e4d38f5879ca417aa7c017410ccf1bf64a640
SamarITS <115398030+SamarITS@users.noreply.github.com>
2023-09-03 10:38:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -200,9 +200,15 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>🗂FileName : <code>{file_name}</code> \n\n🏷FileSize : <code>{file_size}</code>\n\n♻️Join  Now ❤️‍🔥: @SAM_DUB_LEZHa </b>\n\n👀╔════ 🔰𝐉𝐨𝐢𝐧 𝐖𝐢𝐭𝐡 𝐔𝐬🔰 ════╗
-▫️<a href=https://t.me/SAM_DUB_LEZHa> 🗽𝐌𝐚𝐢𝐧 𝐇𝐮𝐛♠</a>
-▫️<a href=https://t.me/SAM_DUB_LEEZZA> ♻️𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩☔​ </a>
+    CAPTION = """<b>🗂FileName : <code>{file_name}</code> 
+
+🏷FileSize : <code>{file_size}</code>
+
+♻️Join  Now ❤️‍🔥: @SAM_DUB_LEZHa </b>
+
+👀╔════ 🔰𝐉𝐨𝐢𝐧 𝐖𝐢𝐭𝐡 𝐔𝐬🔰 ════╗
+▫️<a href=t.me/SAM_DUB_LEZHa> 🗽𝐌𝐚𝐢𝐧 𝐇𝐮𝐛♠</a>
+▫️<a href=t.me/SAM_DUB_LEEZZA> ♻️𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐆𝐫𝐨𝐮𝐩☔​ </a>
 ╚════ 🦞𝐉𝐨𝐢𝐧 𝐖𝐢𝐭𝐡 𝐔𝐬🥀 ════╝</b>""" 
 
     IMDB_TEMPLATE_TXT = """
